febdacdb2ade016e5308888448e6ba3afd853365
Sam W <sam@sam-we.com>
2022-05-03 13:47:38 -0700

    4_script.rb

--- 4_script.rb
+++ 4_script.rb
@@ -5,6 +5,11 @@ require 'debug'
 tot = Dir["data/files/*.pdf"].length
 counter = 1
 Dir.each_child('./data/files') do |file|
+  if file =~ /error/
+    puts "#{counter}/#{tot} Skip"
+    counter += 1
+    next
+  end
   srid = file.sub('.pdf','')
   created_at = JSON.parse(File.open("./data/signature_requests/#{srid}").read)['created_at']
   iso8601 = Time.at(created_at).utc.iso8601
