a0b60762db4e3f0979df6c5cda2220f0a27de4ef
Sam W <sam@sam-we.com>
2022-05-03 12:25:14 -0700

    2_script.rb

--- 2_script.rb
+++ 2_script.rb
@@ -5,7 +5,7 @@ client = HelloSign::Client.new(api_key: ENV['HELLOSIGN_API_KEY'])
 list = client.get_signature_requests(page_size: 99)
 total_pages = list.data["list_info"]["num_pages"]
 puts "total_pages #{total_pages}"
-page = 29
+page = 30
 while page < total_pages + 1
   puts "page #{page}"
   req = client.get_signature_requests(page_size: 98, page: page)
