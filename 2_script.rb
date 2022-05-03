2850945da5abe3021c485ec586d8aa82d68418f0
Sam W <sam@sam-we.com>
2022-05-03 12:14:45 -0700

    2_script.rb

---
+++ 2_script.rb
@@ -0,0 +1,16 @@
+require 'hello_sign'
+require 'debug'
+
+client = HelloSign::Client.new(api_key: ENV['HELLOSIGN_API_KEY'])
+list = client.get_signature_requests(page_size: 99)
+total_pages = list.data["list_info"]["num_pages"]
+page = 1
+while page < total_pages + 1
+  puts "page #{page}"
+  req = client.get_signature_requests(page_size: 98, page: page)
+  req.data["signature_requests"].each do |sr|
+    File.open("data/signature_requests/#{sr['signature_request_id']}", "w") do |file|
+      file.write(sr.to_json)
+    end
+  end
+end
