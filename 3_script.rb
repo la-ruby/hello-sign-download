c461aade509066c4715faa7faad7bdd4d2098d15
Sam W <sam@sam-we.com>
2022-05-03 12:46:53 -0700

    3_script.rb

--- 3_script.rb
+++ 3_script.rb
@@ -2,7 +2,7 @@ require 'hello_sign'
 require 'debug'
 
 client = HelloSign::Client.new(api_key: ENV['HELLOSIGN_API_KEY'])
-r1 = client.signature_request_files :signature_request_id => 'ffaab17218bd96c0a4bb7e3954aa443cbb77a700'
+file_bin = client.signature_request_files :signature_request_id => 'ffaab17218bd96c0a4bb7e3954aa443cbb77a700'
 open("data/files/finalCopy.pdf", "wb") do |file|
   file.write(file_bin)
 end
