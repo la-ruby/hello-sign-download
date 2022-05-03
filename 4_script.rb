0be20b7a29bf0462ff101fc431772b1e7d7ed64a
Sam W <sam@sam-we.com>
2022-05-03 13:40:19 -0700

    4_script.rb

---
+++ 4_script.rb
@@ -0,0 +1,15 @@
+
+tot = Dir["data/files/*"].length
+counter = 1
+Dir.each_child('./data/files') do |file|
+  created_at = JSON.parse(File.open("./data/signature_requests/#{file.gsub(/\.pdf/)}").read)['created_at']
+  iso8601 = Time.at(created_at).utc.iso8601
+  if File.exist?("./data/dated_files/#{iso8601}_#{srid}.pdf")
+    puts "#{counter}/#{tot} Skip #{iso8601}_#{srid}.pdf"
+    counter += 1
+    next
+  end
+  puts "#{counter}/#{tot} #{iso8601}_#{srid}.pdf"
+  count += 1
+end
+
