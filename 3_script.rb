257319f5b9f902094c37197cc7ca40c6cc74253e
Sam W <sam@sam-we.com>
2022-05-03 12:53:07 -0700

    3_script.rb

--- 3_script.rb
+++ 3_script.rb
@@ -3,8 +3,9 @@ require 'debug'
 
 client = HelloSign::Client.new(api_key: ENV['HELLOSIGN_API_KEY'])
 counter = 1
+tot = Dir["data/signature_requests/*"].length
 Dir.each_child('./data/signature_requests') do |srid|
-  puts "Downloading #{counter}/N #{srid}"
+  puts "Downloading #{counter}/#{tot} #{srid}"
   file_bin = client.signature_request_files :signature_request_id => srid
   open("data/files/#{srid}", "wb") do |file|
     file.write(file_bin)
