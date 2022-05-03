a556fb0cb61ee6b822c8628c7ea830d1397423ad
Sam W <sam@sam-we.com>
2022-05-03 12:33:04 -0700

    2_script.rb

--- 2_script.rb
+++ 2_script.rb
@@ -2,13 +2,13 @@ require 'hello_sign'
 require 'debug'
 
 client = HelloSign::Client.new(api_key: ENV['HELLOSIGN_API_KEY'])
-list = client.get_signature_requests(page_size: 100)
+list = client.get_signature_requests(page_size: 99)
 total_pages = list.data["list_info"]["num_pages"]
 puts "total_pages #{total_pages}"
-page = 28
+page = 27
 while page < total_pages + 1
   puts "page #{page}"
-  req = client.get_signature_requests(page_size: 100, page: page)
+  req = client.get_signature_requests(page_size: 99, page: page)
   req.data["signature_requests"].each do |sr|
     next if sr['signature_request_id'].nil? || sr['signature_request_id'].empty?
     puts "Tracing sr['signature_request_id'] #{sr['signature_request_id']}"
