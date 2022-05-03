fb147ecc7e5c160d898af08f85fc603f2ddd4183
Sam W <sam@sam-we.com>
2022-05-03 13:00:29 -0700

    3_script.rb

--- 3_script.rb
+++ 3_script.rb
@@ -5,9 +5,12 @@ client = HelloSign::Client.new(api_key: ENV['HELLOSIGN_API_KEY'])
 counter = 1
 tot = Dir["data/signature_requests/*"].length
 Dir.each_child('./data/signature_requests') do |srid|
-  puts "skip exist #{srid}" if File.exist?("./data/files/#{srid}")
-  next if File.exist?("./data/files/#{srid}")
-  puts "Downloading #{counter}/#{tot} #{srid}"
+  if File.exist?("./data/files/#{srid}")
+    puts "#{counter}/#{tot} Skip #{srid}"
+    counter += 1
+    next
+  end
+  puts "#{counter}/#{tot} Downloading #{srid}"
   file_bin = client.signature_request_files :signature_request_id => srid
   open("./data/files/#{srid}", "wb") do |file|
     file.write(file_bin)
