9b00f74b46a5872bf36d8022d80cf09be85c31b7
Sam W <sam@sam-we.com>
2022-05-03 13:41:17 -0700

    4_script.rb

--- 4_script.rb
+++ 4_script.rb
@@ -3,6 +3,7 @@ require 'json'
 tot = Dir["data/files/*"].length
 counter = 1
 Dir.each_child('./data/files') do |file|
+  debugger
   created_at = JSON.parse(File.open("./data/signature_requests/#{file.gsub(/\.pdf/)}").read)['created_at']
   iso8601 = Time.at(created_at).utc.iso8601
   if File.exist?("./data/dated_files/#{iso8601}_#{srid}.pdf")
